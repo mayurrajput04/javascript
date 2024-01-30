dd5c03b75aedba2a479de47dde20d23617613b44
Mayur Rajput <82179875+mayurrajput04@users.noreply.github.com>
2024-01-30 16:35:58 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,21 @@
+"use strict" // used to write code in newer version
+
+let Name = "Mayur"
+let age = 19
+let accountLogin = false
+
+
+/*
+Datatypes:
+number
+boolean => true/false
+string => "value"
+null => it is a standalone value , it is a typeof "object"
+undefined => fuction having no defined value
+symbol => defines unique
+*/ 
+
+
+console.log(typeof age);
+console.log(typeof null)
+console.log(typeof undefined)
