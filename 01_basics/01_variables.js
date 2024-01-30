22234c568be6bd1dab552faa3167c0ab869db6f3
Mayur Rajput <82179875+mayurrajput04@users.noreply.github.com>
2024-01-30 15:43:55 +0000

    learned variable declaration

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 12345
+let accountEmail = "mayurrajput@04"
+var accountPassword = "password123"
+accountCity = "Chh. Sambhajinagar"
+
+
+//accountId = 23456 // --> This is Not allowed. Assignment to const variable is not allowed
+
+accountEmail = "rajputmayur@google.com"
+accountPassword = "123password"
+let accountName;
+
+console.table([accountId , accountEmail, accountPassword, accountCity, accountName])
+
+/*
+Prefer notto use var
+Because of issue in block scope and funtional scope
+*/
