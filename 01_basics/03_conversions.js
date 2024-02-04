43047dfa8adb3e1b7a500cff0d07a2db18a2a53b
Mayur Rajput <82179875+mayurrajput04@users.noreply.github.com>
2024-02-04 18:06:44 +0000

    datatypes conversions learned

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,28 @@
+let score = "mayur"
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+//String to number conversion:
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+
+
+//string to boolean conversion:
+//  true => 1; false => 0
+let isLoggedIn = "hitesh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+//number to string conversion:
+let someNumber = 3
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
