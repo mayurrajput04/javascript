45860cc5f484f8cdfa3c936a29a2b57ceae2788f
Mayur Rajput <82179875+mayurrajput04@users.noreply.github.com>
2024-02-05 15:21:43 +0000

    conversion of datatypes learned

---
+++ 01_basics/04_converstionoperations.js
@@ -0,0 +1,39 @@
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+//Basic operators:
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); (2 ki power 3)
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+// addition of two strings:
+let str3 = str1 + str2
+// console.log(str3);
+
+//addition of two diff datatypes(integer and strings)
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+//conversion to number:
+// console.log(+true);
+// console.log(+"");
+
+
+//increment operator:
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
