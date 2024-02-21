b6539cc9ed9bae0e621f85bdb6ccc0f5cd9fa6f0
Mayur Rajput <82179875+mayurrajput04@users.noreply.github.com>
2024-02-21 07:33:12 +0000

    basics of dates and time in js

---
+++ 01_basics/08_dates_and_time.js
@@ -0,0 +1,9 @@
+// let myDate = new Date
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toUTCString());
+
+// let myCreatedDate = new Date(2024 ,1 ,20)
+let myCreatedDate = new Date("2-20-2024")
+console.log(myCreatedDate.toLocaleDateString());
+
